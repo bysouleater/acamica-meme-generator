d31ab83d5e64fe21284d7fe7f14850b49e66a00e
Christian Nahuel Balsamo <bysouleater@gmail.com>
2019-10-06 18:44:06 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// Function que se ejecuta cuando el DOM termino de cargar por completo. 
+// https://api.jquery.com/ready/
+$(function() {
+
+  // Guardar en una variable la seleccion de la imagen principal para poder manipularla luego.
+  // Console-loguear la variable para verificar la seleccion.
+  
+
+  // Guardar en una variable la seleccion de todas las imagenes de la lista de memes posibles
+  // Console-loguear la variable para verificar la seleccion.
+  
+  
+  // Manejar el evento click de todas las imagenes de la lista de memes e intercambiar la 
+  // imagen principal con la seleccionada.
+  //
+  // Restricciones: 
+  // - Utilizar la variable anterior
+  // - No se puede eliminar ni crear nuevos elementos de html
+  
+
+  // Guardar en una variable la seleccion del input para el texto superior, y en otra variable 
+  // la seleccion del input para el texto inferior. 
+  // Console-loguear las variables para verificar las selecciones.
+  
+
+  // Guardar en una variable la seleccion del titulo superior del meme, y en otra variable 
+  // la seleccion del titulo inferior del meme. 
+  // Console-loguear las variables para verificar las selecciones.
+  
+
+  // Manejar el evento "input" del input para el texto superior y cambiar el texto a mostrar
+  // en el titulo superior del meme.
+  //
+  // Restricciones:
+  // - Utilizar las variables anteriores
+  // - No se puede eliminar ni crear nuevos elementos de html
+  // - Utilizar .val() para acceder al valor del input
+  
+
+  // Manejar el evento "input" del input para el texto inferior y cambiar el texto a mostrar
+  // en el titulo inferior del meme.
+  //
+  // Restricciones:
+  // - Utilizar las variables anteriores
+  // - No se puede eliminar ni crear nuevos elementos de html
+  // - Utilizar .val() para acceder al valor del input
+  
+
+});
+
